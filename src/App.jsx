bb59a80d1ed89c0b5952d3a19b8216bc89d33fad
Pasan-pramu <pramudithapasan076@gmail.com>
2026-02-15 03:35:55 +0530

    implement feature section

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
            <ProductViewer/>
            <Showcase/>
            <Performance/>
+           <Features/>
            <Highlights/>
            <Footer/>
        </main>
