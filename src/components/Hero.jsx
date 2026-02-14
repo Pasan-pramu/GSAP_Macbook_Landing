e0d91f2ddebc66d5605961a24b0f3cdf06977bda
Pasan-pramu <pramudithapasan076@gmail.com>
2026-02-15 00:59:08 +0530

    implement 3d product view section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import {useEffect, useRef} from "react";
+
+const Hero = () => {
+
+    const videoRef = useRef();
+
+    useEffect(() => {
+        if(videoRef.current)videoRef.current.playbackRate = 2;
+    },[]);
+
+    return (
+       <section id='hero'>
+             <div>
+                 <h1>
+                     Macbook Pro
+                 </h1>
+                 <img src="/title.png" alt="Title" />
+             </div>
+
+           <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+           <button> Buy</button>
+           <p> From $2599 or $259 for 12 months </p>
+       </section>
+    )
+}
+export default Hero
